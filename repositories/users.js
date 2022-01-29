a710636365c6f64484c85de2860e40327f0edf8c
Blake <zabulonov.a@gmail.com>
2022-01-29 13:20:10 +0200

    commit

--- repositories/users.js
+++ repositories/users.js
@@ -7,23 +7,25 @@ const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository extends Repository {
 	async comparePasswords(saved, supplied) {
+		// Saved -> password saved in our database. 'hashed.salt'
+		// Supplied -> password given to us by a user trying sign in
 		const [hashed, salt] = saved.split(".");
 		const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
 
 		return hashed === hashedSuppliedBuf.toString("hex");
 	}
+
 	async create(attrs) {
 		attrs.id = this.randomId();
+
 		const salt = crypto.randomBytes(8).toString("hex");
 		const buf = await scrypt(attrs.password, salt, 64);
 
 		const records = await this.getAll();
-
 		const record = {
 			...attrs,
 			password: `${buf.toString("hex")}.${salt}`,
 		};
-
 		records.push(record);
 
 		await this.writeAll(records);
@@ -33,3 +35,5 @@ class UsersRepository extends Repository {
 }
 
 module.exports = new UsersRepository("users.json");
+
+const test = "test";
