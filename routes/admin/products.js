fc78506a5680c3b9bdbb29a3d7700f05ef47b5fa
Blake <zabulonov.a@gmail.com>
2022-01-29 17:57:10 +0200

    before changes

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -49,12 +49,17 @@ router.post(
 	upload.single("image"),
 	[requireTitle, requirePrice],
 	handleErrors(productsEditTemplate),
+	async (req) => {
+		const product = await productsRepo.getOne(req.params.id);
+		return { product };
+	},
 	async (req, res) => {
 		const changes = req.body;
 
 		if (req.file) {
 			changes.image = req.file.buffer.toString("base64");
 		}
+
 		try {
 			await productsRepo.update(req.params.id, changes);
 		} catch (err) {
