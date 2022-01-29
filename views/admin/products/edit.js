3f39092d514f9ee47cd6d9c911d7278dc7c596bd
Blake <zabulonov.a@gmail.com>
2022-01-29 14:04:07 +0200

    edit img

---
+++ views/admin/products/edit.js
@@ -0,0 +1,15 @@
+const layout = require("../layout");
+
+module.exports = ({ product }) => {
+	return layout({
+		content: `
+    <form method="POST">
+      <input name="title" value="${product.title}" />
+      <input name="price" value="${product.price}" />
+      <input name="price" type="file" />
+      <button>Submit</button>
+      </form>
+    
+    `,
+	});
+};
