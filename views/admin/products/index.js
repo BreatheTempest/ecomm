62538878569e61c658874006120be9cad80f15e0
Blake <zabulonov.a@gmail.com>
2022-01-29 20:47:34 +0200

    admin panel done

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -14,10 +14,12 @@ module.exports = ({ products }) => {
             </button>
           </a>
         </td>
-        <td>
-          <button class="button is-danger">Delete</button>
-        </td>
-      </tr>
+          <td>
+            <form method="POST" action="/admin/products/${product.id}/delete">
+              <button class="button is-danger">Delete</button>
+            </form>
+          </td>
+        </tr>
     `;
 		})
 		.join("");
